94b677193dbde60a94597c357db5526666cbe096
Nita Dake <nitadake95@gamil.com>
2022-07-01 12:18:38 +0530

    SpringProject

---
+++ src/main/java/com/emexo/spring/ioc/beanfactory/Address.java
@@ -0,0 +1,8 @@
+package com.emexo.spring.ioc.beanfactory;
+
+public class Address {
+    private String address="Pune";
+    public void getAddressDetails(){
+        System.out.println("Adrress:"+address);
+    }
+}
